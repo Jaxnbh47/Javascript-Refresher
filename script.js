e557ad115461d13ed656753ff6795c4bbea12e80
Jackson Harris <109924053+Jaxnbh47@users.noreply.github.com>
2022-10-13 19:24:30 -0400

    master

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ console.log(fullName);
 
 let population = "672,351";
 
-console.log(population);
+console.log(3 * population);
+
+let javaScriptIsCool = true;
+
+let colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Violet"];
+
+
+
