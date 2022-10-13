f58be642cab2ff426265c840d11acdd1e3e6f7ce
Jackson Harris <109924053+Jaxnbh47@users.noreply.github.com>
2022-10-13 19:19:02 -0400

    master

---
+++ script.js
@@ -0,0 +1,9 @@
+let firstName = "Jackson";
+let lastName = " Harris";
+let fullName = firstName.concat(lastName);
+
+console.log(fullName);
+
+let population = "672,351";
+
+console.log(population);
